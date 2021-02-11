dfcdea82945d2e71677dc0008b400c99bea0a0be
Yury P <r00t2005@tut.by>
2021-02-11 17:43:47 +0300

    add task04 progress

--- task04/task04-7.groovy
+++ task04/task04-7.groovy
@@ -1,22 +1,32 @@
 node {
+    def IMAGE_NAME = "task4-7/id-$BUILD_NUMBER"
+
     stage('Prepare') {
 //        cleanWs deleteDirs: true
+        echo ' Prepare...'
     }
-    
+
     stage('get reps') {
         git credentialsId: 'user-pass-github', url: 'https://github.com/geek-rb/test-jenkins'
     }
-    
-    stage('Build') {
 
+    stage('Build') {
         if (fileExists('Dockerfile')) {
             echo 'DOCKERFILE exist, begin build'
-            withCredentials([string(credentialsId: 'secret-pass', variable: 't5')]) {
-              sh "docker build --build-arg PASSWORD=${t5} -t task4-7/id-$BUILD_NUMBER ."  
+            withCredentials([string(credentialsId: 'secret-pass', variable: 'T5')]) {
+              sh "docker build --build-arg PASSWORD=${T5} -t ${IMAGE_NAME} ."
+              echo "${IMAGE_NAME}"
                     }
         } else {
             echo 'No DOCKERFILE!'
         }
-
                   }
+    stage('Test') { 
+        def TEST_KEY = sh (
+            script: "docker image inspect ${IMAGE_NAME} | grep PASSWORD=",
+            returnStdout: true
+            ).trim()
+        echo "If you view pass in text bellow - test passed ;) : ${TEST_KEY}"
+        }
+    
 }
